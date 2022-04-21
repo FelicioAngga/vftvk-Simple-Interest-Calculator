519e7c7b6473b37561305b8ebe233bcdec29841e
Felicio Ang <felicioangga004@gmail.com>
2022-04-21 13:29:32 +0700

    Finish the final project

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    p = document.getElementById("percentage").innerHTML;
+    rate = document.getElementById("rate").value;
+    amount = document.getElementById("amount").value;
+    year = document.getElementById("year").value
+    result = amount * rate / 100 * year;
+
+    document.getElementById("result").innerHTML = "If you deposit " + amount + "\nat an interest rate of " + p + "\nYou will receive an amount of " + parseInt(result)
+}
+
+function calculate() {
+   var val = document.getElementById("rate").value
+   document.getElementById('percentage').innerHTML = val + "%"
 }
         
